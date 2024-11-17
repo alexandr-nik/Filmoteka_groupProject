a91cdddbc6df776465f0083c09488e372cfb7f90
Alexandr Nik <107132921+alexandr-nik@users.noreply.github.com>
2024-11-17 15:19:33 +0200

    Update menuFilters.js

--- src/js/menuFilters.js
+++ src/js/menuFilters.js
@@ -6,7 +6,7 @@ import { pagination } from './pagination';
 import ApiService from './fetchProdactsAPI';
 // import { resetQuery } from './searchFilms';
 const apiService = new ApiService();
-const KEY = '32432509d17cea42104bbb7507a382c7';
+const KEY = '8d41be39d2696eeff12b89d916a6c984';
 const api_key = `?api_key=${KEY}`;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
